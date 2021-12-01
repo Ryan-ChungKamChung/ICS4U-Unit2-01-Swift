5b1aeddfebfb89d24dd031b3f6f9cb5a752ac50f
Ubuntu <ubuntu@ip-172-31-92-172.ec2.internal>
2021-12-01 04:58:19 +0000

    fixed

--- ReverseString.swift
+++ ReverseString.swift
@@ -1,29 +1,32 @@
 //
-//  DiceGame.swift
+//  ReverseString.swift
 //
 //  Created by Ryan Chung
-//  Created on 2021-11-18
+//  Created on 2021-11-30
 //  Version 1.0
 //  Copyright (c) 2020 Ryan Chung. All rights reserved.
 //
-//  This program generates a random number and asks the user to guess that
-//  number.
+//  This program reverses a user-inputted string.
 //
 
 import Foundation
 
+// Reverses an inputted string via recursion
 func reverse(inputString: String) -> String {
 
     if inputString.count == 0 {
         return inputString
     } else {
-        return inputString[1...] + inputString[0...]
+        let range = inputString
+            .index(after: inputString.startIndex)..<inputString.endIndex
+        return reverse(inputString: String(inputString[range]))
+            + inputString.prefix(1)
     }
 }
 
+// Captures user input, feeds it to reverse() and outputs the reversed string
 print("Enter a string: ", terminator: "")
 let inputString = readLine()
-
 let reversedString = reverse(inputString: inputString!)
 print(reversedString)
 
